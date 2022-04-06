f638f1ad7e3d0170b635c65532651d6bb88c5003
Eliza Khachatryan <elijbet@gmail.com>
2022-04-06 08:32:50 -0700

    concept of Proxy

--- src/builder.js
+++ src/builder.js
@@ -35,8 +35,24 @@ export default class Launcher {
       default:
         throw new Error('Supported devices are only MOBILE | TABLET | DESKTOP')
     }
+    // PROXY
+    // const target = {
+    //   message1: 'hello',
+    //   message2: 'everyone',
+    // }
+    // const handler2 = {
+    //   get(target, prop, receiver) {
+    //     return 'world'
+    //   },
+    // }
+    // const proxy2 = new Proxy(target, handler2)
+    // console.log(proxy2.message1); // world
+    // console.log(proxy2.message2); // world
+
+    // Here we've provided an implementation of the get() handler, which intercepts attempts to access properties in the target. Handler functions are sometimes called traps, presumably because they trap calls to the target object.
 
     return new Proxy(extendedPage, {
+      // proxy just takes multiple objects and merges them into one
       get: function (_target, property) {
         return extendedPage[property] || browser[property] || page[property]
       },
